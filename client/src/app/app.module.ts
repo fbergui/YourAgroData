03745f25b47664c68b49179d28566ef9d1ad2463
fbergui <f.bergui.1268@vallauri.edu>
2023-03-16 18:31:19 +0100

    body of homepage idea

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,19 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { HomeComponent } from './body/home/home.component';
+import { AboutComponent } from './body/about/about.component';
+import { ServicesPageComponent } from './body/services-page/services-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesPageComponent,
   ],
   imports: [
     BrowserModule,
